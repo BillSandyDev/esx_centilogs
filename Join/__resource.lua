635c098967adafee65bc4bafe974f85bf254d7a8
BillSandyDev <131354966+BillSandyDev@users.noreply.github.com>
2023-04-20 14:54:09 +0300

    Delete __resource.lua

--- Join/__resource.lua
+++
@@ -1,9 +0,0 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
-description "DISCORD & FIVEM LOGS || JOIN"      -- DESCRIPTION ENTRY
-url_link "https://github.com/BillSandyDev/esx_centilogs/tree/main"        -- LINK ENTRY
-autor 'Centified !G'        -- AUTOR ENTRY
-
-
-server_scripts {         -- SERVER SCRIPTS
-	'logger_sv.lua',
-}
